d950ec4621ec17006b192ce5b487a73726197ec5
Wolfsion <jison.c9@qq.com>
2023-07-26 15:20:45 +0800

    'ch-round'

--- testProc.py
+++ testProc.py
@@ -17,8 +17,22 @@ def _get_other_mask(logits, target):
 if __name__ == "__main__":
     # from dl.test_unit import main
     # main()
-    a = torch.ones(10)
-    b = torch.div(a, 10)
-    print(a)
-    print(b)
+    loss_func = torch.nn.CrossEntropyLoss(reduction="none")
+    pre = torch.tensor([[0.1, 0.5, 0.3, 0.4],
+                        [0.1, 0.5, 0.3, 0.4],
+                        [0.1, 0.5, 0.3, 0.4]], dtype=torch.float)
+    tgt = torch.tensor([[1, 0, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 0, 1, 0]], dtype=torch.float)
+    print("手动计算:")
+    print("1.softmax")
+    print(torch.softmax(pre, dim=-1))
+    print("2.取对数")
+    print(torch.log(torch.softmax(pre, dim=-1)))
+    print("3.与真实值相乘")
+    print(-torch.sum(torch.mul(torch.log(torch.softmax(pre, dim=-1)), tgt), dim=-1))
+    print()
+    print("调用损失函数:")
+    loss = loss_func(pre, tgt)
+    print(loss)
     print("----------------------")
