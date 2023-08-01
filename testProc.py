33a0daf1f7ce22569b226173a5d4a2fe6984eda5
Wolfsion <jison.c9@qq.com>
2023-08-01 20:09:03 +0800

    'softmax'

--- testProc.py
+++ testProc.py
@@ -1,8 +1,11 @@
 import numpy as np
+import torch
 
 if __name__ == "__main__":
     # from dl.test_unit import main
     # main()
-    a = np.argsort(np.array([1, 3, 4, 5, 2])).tolist()
-    print(a)
+    a = torch.ones(3, 5)
+    a[2][2] = 0
+    print(torch.nn.functional.log_softmax(a, dim=1))
+    print(torch.nn.functional.softmax(a, dim=1))
     print("----------------------")
